46c02d1901f700dbba8dc75fc2bbbd60466632f0
jialin.zhou <jialin.zhouz@qq.com>
2024-05-14 10:54:30 +0800

    框架搭建

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,11 @@
+package com.nageoffer.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
