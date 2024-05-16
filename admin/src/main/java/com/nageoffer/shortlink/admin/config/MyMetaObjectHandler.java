ecd2c8aa42078949fc2ee47e25fa77e3e1e06a1c
jialin.zhou <jialin.zhouz@qq.com>
2024-05-16 22:11:44 +0800

    feature：用户注册时部分字段自动更新

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
