fc722043d5cc5a6501ee923f874f9c61dad165ed
jialin.zhou <jialin.zhouz@qq.com>
2024-05-17 15:28:39 +0800

    feature：redis缓存常量类-分布式锁

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,9 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管系统 Redis 缓存常亮类
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_REGISTER_KEY = "shortLink:lock_user-register:";
+}
