5072c53f58b634c3d1263e37d30c079494628c17
jialin.zhou <jialin.zhouz@qq.com>
2024-05-17 14:34:57 +0800

    feature：开发短链接后管系统通用持久层实体对象属性填充

--- admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -5,21 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Slf4j
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
         this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
 
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
     }
 }
