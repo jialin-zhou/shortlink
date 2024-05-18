bc8596493dd25ad22b2618fb0a993b3f327eeca6
jialin.zhou <jialin.zhouz@qq.com>
2024-05-18 20:26:09 +0800

    feature: 短链接后管系统 初始化短链接分组功能代码

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -0,0 +1,12 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接分组控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class GroupController {
+}
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -0,0 +1,16 @@
+package com.nageoffer.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
+import com.nageoffer.shortlink.admin.service.GroupService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 短链接分组实现层
+ */
+@Slf4j
+@Service
+public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+}
