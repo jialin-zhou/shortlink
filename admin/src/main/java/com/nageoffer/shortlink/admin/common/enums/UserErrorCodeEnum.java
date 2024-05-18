e6efbe157f68560a0ba13aa7a6a799f2924c2fa0
jialin.zhou <jialin.zhouz@qq.com>
2024-05-18 16:05:47 +0800

    feature: 短链接后管系统 用户退出登录功能中 用户未登录错误码

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -4,12 +4,13 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    USER_NULL("B000200", "用户不存在"),
+    USER_NULL_ERROR("B000200", "用户不存在"),
     USER_EXIST_ERROR("B000201", "用户名已存在"),
     PASSWORD_SHORT_ERROR("B000202", "密码长度错误"),
     USER_SAVE_ERROR("B000203", "用户记录新增失败"),
     USER_REGISTER_LOCK_ERROR("B000204", "用户注册锁获取失败"),
-    USER_LOGIN_ERROR("B000205", "用戶已经登录");
+    USER_ALREADY_LOGIN_ERROR("B000205", "用戶已经登录"),
+    USER_NOT_LOGIN_ERROR("B000206", "用戶未登录");
 
     private final String code;
 
