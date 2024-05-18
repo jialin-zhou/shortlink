7bbc3e362a6fed87c950f03284ad786a35a14d22
jialin.zhou <jialin.zhouz@qq.com>
2024-05-18 09:36:54 +0800

    feature: 短链接后管系统 redis用户注册分布式锁名更新

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -5,5 +5,5 @@ package com.nageoffer.shortlink.admin.common.constant;
  */
 public class RedisCacheConstant {
 
-    public static final String LOCK_USER_REGISTER_KEY = "shortLink:lock_user-register:";
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
 }
