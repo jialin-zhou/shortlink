75005ea350a9a15343e9c2f69e0e512ec95550ce
jialin.zhou <jialin.zhouz@qq.com>
2024-05-18 10:02:54 +0800

    feature: 短链接后管系统 用户个人信息修改功能-对应的请求参数

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/req/UserUpdateReqDTO.java
@@ -0,0 +1,32 @@
+package com.nageoffer.shortlink.admin.dto.req;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateReqDTO {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
