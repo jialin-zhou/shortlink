3124309615c23f44e9eca890b56b230b6d23e83d
jialin.zhou <jialin.zhouz@qq.com>
2024-05-22 22:00:20 +0800

    feature: 短链接中台系统 短链接中台管理MVC基本框架

---
+++ project/src/main/java/com/nageoffer/shortlink/project/ShortLinkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nageoffer.shortlink.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.nageoffer.shortlink.project.dao.mapper")
+public class ShortLinkProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkProjectApplication.class, args);
+    }
+}
---
+++ project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
@@ -0,0 +1,31 @@
+package com.nageoffer.shortlink.project.controller;
+
+import com.nageoffer.shortlink.project.common.convention.result.Result;
+import com.nageoffer.shortlink.project.common.convention.result.Results;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.project.service.ShortLinkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class ShortLinkController {
+
+    private final ShortLinkService shortLinkService;
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+//        HttpRequest httpRequest = HttpUtil.createPost("http://127.0.0.1:8001/api/short-link/v1/create").body(JSON.toJSONString(requestParam));
+//        HttpResponse execute = httpRequest.execute();
+        return Results.success(shortLinkService.createShortLink(requestParam));
+    }
+}
