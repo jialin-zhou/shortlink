16e59728216a89003c65a710745abc78d031b4ab
jialin.zhou <jialin.zhouz@qq.com>
2024-05-22 21:58:16 +0800

    feature: 短链接中台系统 错误码+系统报错提示

---
+++ project/src/main/java/com/nageoffer/shortlink/project/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,13 @@
+package com.nageoffer.shortlink.project.common.convention.errorcode;
+
+public interface IErrorCode {
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
