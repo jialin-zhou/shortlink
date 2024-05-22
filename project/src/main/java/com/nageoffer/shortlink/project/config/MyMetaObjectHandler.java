fb5388bf9f3a8f77fcaad70cbccd4afda9024744
jialin.zhou <jialin.zhouz@qq.com>
2024-05-22 22:01:31 +0800

    feature: 短链接中台系统 实现更新、创建短链接时数据库部分参数系统自动配置

---
+++ project/src/main/java/com/nageoffer/shortlink/project/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.project.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
