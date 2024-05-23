f911f668acc0e878b8746aa76248bd8932c29138
jialin.zhou <jialin.zhouz@qq.com>
2024-05-23 22:09:06 +0800

    feature: 短链接中台系统 短链接分页查询请求参数和响应参数

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,16 @@
+package com.nageoffer.shortlink.project.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
+import lombok.Data;
+
+/**
+ * 短链接分组请求参数
+ */
+@Data
+public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
