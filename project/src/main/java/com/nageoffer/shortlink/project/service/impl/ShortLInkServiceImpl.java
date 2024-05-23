1639edb33af4d100c07f67faaa481e40b69b2d07
jialin.zhou <jialin.zhouz@qq.com>
2024-05-23 19:41:09 +0800

    feature: 短链接中台系统 实现创建短链接功能

--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLInkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLInkServiceImpl.java
@@ -1,14 +1,20 @@
 package com.nageoffer.shortlink.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.project.common.convention.exception.ServiceException;
 import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
 import com.nageoffer.shortlink.project.dao.mapper.LinkMapper;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.nageoffer.shortlink.project.service.ShortLinkService;
 import com.nageoffer.shortlink.project.toolkit.HashUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,15 +22,35 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ShortLInkServiceImpl extends ServiceImpl<LinkMapper, ShortLinkDO> implements ShortLinkService {
 
+    private final RBloomFilter<String> shortUrlCreateRegisterCachePenetrationBloomFilter;
+
+    /**
+     * 生成短链接
+     * @param requestParam 创建短链接请求参数
+     * @return 创建短链接响应参数
+     */
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
         String shortLinkSuffix = generateSuffix(requestParam);
+        String fullShortUrl = requestParam.getDomain() + "/" + shortLinkSuffix;
         ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
         shortLinkDO.setShortUri(shortLinkSuffix);
-        shortLinkDO.setFullShortUrl(requestParam.getDomain() + "/" + shortLinkSuffix);
-        baseMapper.insert(shortLinkDO);
+        shortLinkDO.setFullShortUrl(fullShortUrl);
+        try {
+            baseMapper.insert(shortLinkDO);
+        } catch (DuplicateKeyException ex) {
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO hasShortLink = baseMapper.selectOne(queryWrapper);
+            if (hasShortLink != null) {
+                log.warn("短链接重复生成，fullShortUrl:{}", fullShortUrl);
+                throw new ServiceException("短链接重复生成");
+            }
+        }
+        shortUrlCreateRegisterCachePenetrationBloomFilter.add(requestParam.getDomain() + "/" + shortLinkSuffix);
         return ShortLinkCreateRespDTO.builder()
                 .fullShortUrl(shortLinkDO.getFullShortUrl())
                 .originUrl(requestParam.getOriginUrl())
@@ -32,8 +58,27 @@ public class ShortLInkServiceImpl extends ServiceImpl<LinkMapper, ShortLinkDO> i
                 .build();
     }
 
+    /**
+     * 根据哈希算法生成短链接
+     * @param requestParam 创建短链接请求参数
+     * @return 生成的短链接
+     */
     private String generateSuffix(ShortLinkCreateReqDTO requestParam) {
         String originUrl = requestParam.getOriginUrl();
-        return HashUtil.hashToBase62(originUrl);
+        String shortUri;
+
+        int customGenerateCount = 0;
+        while (true) {
+            if (customGenerateCount > 10) {
+                throw new ServiceException("短链接频繁生成，请稍后再试试");
+            }
+            originUrl += System.currentTimeMillis();
+            shortUri = HashUtil.hashToBase62(originUrl);
+            if ( !shortUrlCreateRegisterCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri)) {
+                break;
+            }
+            customGenerateCount++;
+        }
+        return shortUri;
     }
 }
