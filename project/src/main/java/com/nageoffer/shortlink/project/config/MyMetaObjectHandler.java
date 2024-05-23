c605e89a114b4896328fd2a876ba2b5f4778758c
jialin.zhou <jialin.zhouz@qq.com>
2024-05-23 16:01:52 +0800

    feature: 短链接中台系统 新增注释

--- project/src/main/java/com/nageoffer/shortlink/project/config/MyMetaObjectHandler.java
+++ project/src/main/java/com/nageoffer/shortlink/project/config/MyMetaObjectHandler.java
@@ -10,16 +10,34 @@ import java.util.Date;
 @Slf4j
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
+    /**
+     * 插入数据时自动填充创建时间和更新时间，以及设置删除标志位。
+     *
+     * @param metaObject 元对象，代表待插入的数据对象。
+     * 无返回值，但会对传入的metaObject进行操作，自动填充createTime、updateTime和delFlag字段。
+     */
     @Override
     public void insertFill(MetaObject metaObject) {
+        // 填充创建时间和更新时间为当前时间
         this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
         this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        // 填充删除标志位为0，表示未删除
         this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
 
     }
 
+
+    /**
+     * 更新填充方法。
+     * 此方法会为指定的元对象填充更新时间字段。
+     *
+     * @param metaObject 元对象，即将被更新的对象。
+     * 注意：此方法不返回任何值，它对传入的元对象进行修改。
+     */
     @Override
     public void updateFill(MetaObject metaObject) {
+        // 使用严格更新填充方式，为元对象设置"updateTime"字段的值，使用系统当前时间
         this.strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
     }
+
 }
--- project/src/main/java/com/nageoffer/shortlink/project/toolkit/HashUtil.java
+++ project/src/main/java/com/nageoffer/shortlink/project/toolkit/HashUtil.java
@@ -13,19 +13,40 @@ public class HashUtil {
     };
     private static final int SIZE = CHARS.length;
 
+    /**
+     * 将十进制数转换为62进制表示的字符串。
+     * 使用一个预定义的字符集（CHARS），包含0-9，A-Z，a-z共62个字符。
+     *
+     * @param num 需要转换的十进制数。
+     * @return 转换后的62进制字符串。
+     */
     private static String convertDecToBase62(long num) {
         StringBuilder sb = new StringBuilder();
+        // 通过取余数的方式，从低位到高位逐步构建62进制字符串
         while (num > 0) {
-            int i = (int) (num % SIZE);
-            sb.append(CHARS[i]);
-            num /= SIZE;
+            int i = (int) (num % SIZE); // 取余数
+            sb.append(CHARS[i]); // 将余数对应的字符追加到字符串中
+            num /= SIZE; // 更新num为商，继续下一轮迭代
         }
+        // 字符串逆序后返回
         return sb.reverse().toString();
     }
 
+
+    /**
+     * 将字符串转换为基于Base62编码的字符串。
+     * 此方法首先使用MurmurHash算法对输入字符串进行哈希，然后将得到的哈希值转换为Base62编码。
+     *
+     * @param str 需要进行Base62编码的字符串。
+     * @return 返回基于Base62编码的字符串。
+     */
     public static String hashToBase62(String str) {
+        // 使用MurmurHash32算法获取字符串的哈希值
         int i = MurmurHash.hash32(str);
+        // 将哈希值转换为Long类型，并确保它是正数
         long num = i < 0 ? Integer.MAX_VALUE - (long) i : i;
+        // 将转换后的正整数转换为Base62编码字符串
         return convertDecToBase62(num);
     }
+
 }
