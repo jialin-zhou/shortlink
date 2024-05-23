9234d9447b471cdd744020d109c22d73a88b02b0
jialin.zhou <jialin.zhouz@qq.com>
2024-05-23 22:08:20 +0800

    feature: 短链接表分页，新增favicon字段

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -24,6 +24,7 @@ public class UserTableShardingTest {
             "`origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
             "`click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
             "`gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "`favicon` varchar(256) DEFAULT NULL COMMENT '网站图标',\n" +
             "`enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',\n" +
             "`created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',\n" +
             "`valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
