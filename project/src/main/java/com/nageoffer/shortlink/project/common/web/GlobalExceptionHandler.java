cf4766960f074d070e5f922fdcc4950db25fc999
jialin.zhou <jialin.zhouz@qq.com>
2024-05-23 19:40:21 +0800

    feature: 短链接中台系统 全局异常处理

---
+++ project/src/main/java/com/nageoffer/shortlink/project/common/web/GlobalExceptionHandler.java
@@ -0,0 +1,86 @@
+package com.nageoffer.shortlink.project.common.web;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import com.nageoffer.shortlink.project.common.convention.errorcode.BaseErrorCode;
+import com.nageoffer.shortlink.project.common.convention.exception.AbstractException;
+import com.nageoffer.shortlink.project.common.convention.result.Result;
+import com.nageoffer.shortlink.project.common.convention.result.Results;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+/**
+ * 全局异常处理器
+ *
+ */
+@Component
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 在 Spring Boot 的异常处理中，每个由 @ExceptionHandler 注解的方法通常需要传递至少两个参数：
+     * 一个是引起异常的请求的信息（如 HttpServletRequest），另一个是实际抛出的异常对象。这种设计有几个重要的理由:
+     * 1、访问请求信息 (HttpServletRequest) 提供了导致异常的请求的详细信息，如请求方法、请求URL、查询参数等。这些信息对于理解异常的上下文非常关键，尤其是在记录日志或者进行问题跟踪时
+     * 2、异常对象 (Exception, Throwable 等。异常对象携带了抛出异常时的详细错误信息和堆栈跟踪。这些信息对于诊断问题至关重要
+     */
+
+    /**
+     * 拦截参数验证异常
+     */
+    @SneakyThrows
+    @ExceptionHandler(value = MethodArgumentNotValidException.class) // 指定拦截的异常类型，处理参数校验的异常
+    public Result validExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError firstFieldError = CollectionUtil.getFirst(bindingResult.getFieldErrors());
+        String exceptionStr = Optional.ofNullable(firstFieldError)
+                .map(FieldError::getDefaultMessage)
+                .orElse(StrUtil.EMPTY);
+        log.error("[{}] {} [ex] {}", request.getMethod(), getUrl(request), exceptionStr);
+        return Results.failure(BaseErrorCode.CLIENT_ERROR.code(), exceptionStr);
+    }
+
+    /**
+     * 拦截应用内抛出的异常
+     */
+    // 指定拦截的异常类型，指定的是AbstractException，拦截所有继承自AbstractException的异常
+    @ExceptionHandler(value = {AbstractException.class})
+    public Result abstractException(HttpServletRequest request, AbstractException ex) {
+        if (ex.getCause() != null) {
+            log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString(), ex.getCause());
+            return Results.failure(ex);
+        }
+        log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString());
+        return Results.failure(ex);
+    }
+
+    /**
+     * 拦截未捕获异常
+     *
+     * 一个通用的异常处理器，处理所有其他未被前面方法处理的异常
+     * Throwable 是所有错误和异常的超类
+     * 使用 Throwable.class 在 @ExceptionHandler 注解中意味着这个处理器能够捕获所有类型的异常和错误，包括 Exception 和 Error 的所有子类。
+     */
+    @ExceptionHandler(value = Throwable.class)
+    public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
+        log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        return Results.failure();
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
