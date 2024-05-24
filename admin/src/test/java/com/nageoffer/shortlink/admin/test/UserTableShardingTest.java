2a669afde8697cfc15c3b927e40d9998730aada8
jialin.zhou <jialin.zhouz@qq.com>
2024-05-24 10:56:17 +0800

    feature: 短链接管理系统 短链接分组表分表

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -36,9 +36,23 @@ public class UserTableShardingTest {
             "PRIMARY KEY (`id`), UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 
+    public static final String SQL_GROUP = "create table `t_group_%d`\n" +
+            "(\n" +
+            "    `id`          bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "    `gid`         varchar(32)  DEFAULT NULL COMMENT '分组标识',\n" +
+            "    `name`        varchar(64)  DEFAULT NULL COMMENT '分组名称',\n" +
+            "    `username`    varchar(256) DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "    `sort_order`  int(3)        DEFAULT NULL COMMENT '分组排序',\n" +
+            "    `create_time` datetime    DEFAULT NULL COMMENT '创建时间',\n" +
+            "    `update_time` datetime    DEFAULT NULL COMMENT '修改时间',\n" +
+            "    `del_flag`    tinyint(1)   DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "    PRIMARY KEY (`id`),\n" +
+            "    UNIQUE KEY `idx_unique_username_gid` (gid, username) USING BTREE\n" +
+            ")ENGINE=InnoDB AUTO_INCREMENT=1716734146606301186 DEFAULT CHARSET=utf8mb4;";
+
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++){
-            System.out.printf((SQL_LINK) + "%n", i);
+            System.out.printf((SQL_GROUP) + "%n", i);
         }
     }
 }
