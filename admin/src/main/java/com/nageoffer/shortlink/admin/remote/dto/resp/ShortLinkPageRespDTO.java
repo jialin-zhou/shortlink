9d149db02dc075ac2ccefcc7a1cce259000c966f
jialin.zhou <jialin.zhouz@qq.com>
2024-05-24 10:17:43 +0800

    feature: 短链接后管系统调用中台 请求参数和响应参数

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/resp/ShortLinkPageRespDTO.java
@@ -0,0 +1,77 @@
+package com.nageoffer.shortlink.admin.remote.dto.resp;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 短链接分页返回参数
+ */
+@Data
+public class ShortLinkPageRespDTO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUri;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 有效期类型 0：永久有效 1：用户自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 启用标识 （0：启用）（1：未启用）
+     */
+    private Integer enableStatus;
+
+    /**
+     * 有效期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 描述
+     */
+    @TableField("`describe`")
+    private String describe;
+
+    /**
+     * 网站标识
+     */
+    private String favicon;
+}
