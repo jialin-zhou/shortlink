66c56f8199f986ce9c42403573d7047314c0e8b2
jialin.zhou <1597709078@qq.com>
2024-08-14 16:33:21 +0800

    fix: 前端本地启动

--- console-vue/vite.config.js
+++ console-vue/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8002',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
